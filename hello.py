583012e92ebde182abc2d1e6c9c78df8b4dc4b46
ashlygeorge1202 <ashlygeorge1202@gmail.com>
2025-11-19 15:23:06 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello, World from Render!"
+    return "Hello World from Render 🎉"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
